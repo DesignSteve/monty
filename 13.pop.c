55903638662ad401d4fa5206e400741a295c4ac0
DesignSteve <steveboss008@gmail.com>
2023-12-14 20:32:16 -0800

    monty

--- 13.pop.c
+++ 13.pop.c
@@ -1,21 +1,6 @@
 #include "monty.h"
 
 /**
-  * pop_error - Prints error messages for "pop"
-  * operation on empty stacks.
-  *
-  * @line_number: Line number in the script where
-  * the error occurred.
-  *
-  * Return: Always returns (EXIT_FAILURE).
-  */
-int pop_error(unsigned int line_number)
-{
-	fprintf(stderr, "L%u: can't release an empty stack\n", line_number);
-	return (EXIT_FAILURE);
-}
-
-/**
  * _pop - Removes the top element from
  * a stack_t linked list.
  *
@@ -40,4 +25,19 @@ void _pop(stack_t **stack, unsigned int line_number)
 	if (next)
 		next->prev = *stack;
 	(*stack)->next = next;
+}
+
+/**
+  * pop_error - Prints error messages for "pop"
+  * operation on empty stacks.
+  *
+  * @line_number: Line number in the script where
+  * the error occurred.
+  *
+  * Return: Always returns (EXIT_FAILURE).
+  */
+int pop_error(unsigned int line_number)
+{
+	fprintf(stderr, "L%u: can't release an empty stack\n", line_number);
+	return (EXIT_FAILURE);
 }
